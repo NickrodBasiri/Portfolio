720dbd6ddcd7e3f460ec4c752846a5fc3a628333
NickrodBasiri <basirinickrod@gmail.com>
2024-11-03 17:08:45 -0500

    Added icons.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,38 @@
 import '../styles/about.css';
+import githubPFP from '../assets/githubPFP.png'
+import htmlIcon from '../assets/icons/html.png'
+import cssIcon from '../assets/icons/css.png'
+import javascriptIcon from '../assets/icons/javascript.png'
+import typescriptIcon from '../assets/icons/typescript.png'
+import javaIcon from '../assets/icons/java.png'
+import gitIcon from '../assets/icons/git.png'
+import pythonIcon from '../assets/icons/python.png'
+import reactIcon from '../assets/icons/react.png'
+import tailwindIcon from '../assets/icons/tailwind.png'
 
 export default function About() {
     return (
         <>
             <div id="info-grid">
                 <div id="personal" className="grid-box">
-                    <h1>picture</h1>
+                    <img id="profile" src={githubPFP}></img>
                     <h1>Nickrod Basiri</h1>
                 </div>
-                <div id="tech" className="grid-box">tech</div>
+                <div id="info-card" className="grid-box">Student & Full Stack Developer</div>
+                <div id="tech" className="grid-box">
+                    <h1>Tech I Use</h1>
+                    <div id="tech-box">
+                        <img className="icon" src={htmlIcon}></img>
+                        <img className="icon" src={cssIcon}></img>
+                        <img className="icon" src={gitIcon}></img>
+                        <img className="icon" src={javascriptIcon}></img>
+                        <img className="icon" src={typescriptIcon}></img>
+                        <img className="icon" src={reactIcon}></img>
+                        <img className="icon" src={javaIcon}></img>
+                        <img className="icon" src={pythonIcon}></img>
+                        <img className="icon" src={tailwindIcon}></img>
+                    </div>
+                </div>
                 <div id="contact" className="grid-box">contact</div>
                 <div id="about" className="grid-box">about me</div>
             </div>
