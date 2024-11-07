30c87d88217ab93c3e19527e753d62bf867e2d0b
nickrodbasiri <basirinickrod@gmail.com>
2024-11-06 22:37:41 -0500

    organized code.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,17 +13,17 @@ import tailwindIcon from '../assets/icons/tailwind.png'
 export default function About() {
     return (
         <>
-            <div id="info-grid">
-                <div id="personal" className="grid-box">
-                    <img id="profile" src={githubPFP}></img>
+            <div id="info-grid" className='grid gap-[1em] w-[60vw]'>
+                <div id="personal" className="grid-box opacity-0 text-center">
+                    <img id="profile" className='rounded-full object-cover' src={githubPFP}></img>
                     <h1>Nickrod Basiri</h1>
                 </div>
-                <div id="info-card" className="grid-box">
+                <div id="info-card" className="grid-box opacity-0 text-center">
                     <p>Full Stack Developer</p>
                 </div>
-                <div id="tech" className="grid-box">
+                <div id="tech" className="grid-box opacity-0 flex flex-col justify-evenly">
                     <h1>Tech I Use</h1>
-                    <div id="tech-box">
+                    <div id="tech-box" className='grid place-items-center grid-cols-3 grid-rows-3'>
                         <img className="icon" src={htmlIcon}></img>
                         <img className="icon" src={cssIcon}></img>
                         <img className="icon" src={gitIcon}></img>
@@ -35,20 +35,18 @@ export default function About() {
                         <img className="icon" src={tailwindIcon}></img>
                     </div>
                 </div>
-                <div id="resume" className='grid-box'>
-                    <button onClick={() => window.open('/resume.pdf', '_blank')} className="resume-button">
+                <div id="resume" className='grid-box opacity-0'>
+                    <button onClick={() => window.open('/resume.pdf', '_blank')} className="resume-button bg-none text-white rounded-lg border-2 border-gray-300">
                         Resume
                     </button>
                 </div>
-                <div id="contact" className="grid-box">
+                <div id="contact" className="grid-box opacity-0 flex flex-col">
                     <a href="https://github.com/NickrodBasiri" target="_blank"><i className="contact-icon visited:text-white fa-brands fa-square-github"/></a>
                     <a href="https://www.linkedin.com/in/nickrodbasiri/" target="_blank"><i className="contact-icon visited:text-white fa-brands fa-linkedin"/></a>
                     <a href="mailto:basirinickrod@gmail.com"><i className="contact-icon visited:text-white fa-envelope fa-solid"/></a>
                 </div>
-                <div id="about" className="grid-box">Hi! I'm Nickrod Basiri, a second-year Software Engineering student at York University. I'm passionate about continuous learning and using technology to tackle real-world problems, always striving to make a positive impact.</div>
+                <div id="about" className="grid-box opacity-0">Hi! I'm Nickrod Basiri, a second-year Software Engineering student at York University. I'm passionate about continuous learning and using technology to tackle real-world problems, always striving to make a positive impact.</div>
             </div>
         </>
     )
 }
-
-<a href="/resume.pdf" target="_blank"><i className="contact-icon fa-file fa-regular"/></a>
