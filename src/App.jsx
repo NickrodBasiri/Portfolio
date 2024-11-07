5c84c426259d4d5e0b5d54a64c8c1bdaa354a5cc
NickrodBasiri <basirinickrod@gmail.com>
2024-11-07 16:52:32 -0500

    project-card styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,19 @@ import Project from "./components/Project"
 function App() {
   return (
     <>
-        <div id="container" className="flex flex-col justify-center items-center">
-          <About/>
-          <Project
-            title="Resume Roast"
-            description="Resume Roast takes the user's resume, and uses AI and voice replication systems to generate a track by Drake, dissing the resume in a personalized and playful manner. Drake will lay down some smooth bars and sharp lyrics highlighting the weaknesses in your resume. After listening to the exceptional track by Toronto's very own, the user is then prompted to fix their resume. They are then treated to yet another musical masterpiece by Drake's biggest rival, Kendrick Lamar. This is where the user gains key insights on specific ways to improve their resume."
-            image="/resumeroast.png"
-            tech="react"/>
+        <div id="container">
+          <section className="flex flex-col justify-center items-center min-h-screen">
+            <About/>
+          </section>
+
+          <section className="flex flex-col justify-start items-center gap-4 pt-8 min-h-screen">
+            <h1 className="mb-8 text-7xl text-white">Projects</h1>
+            <Project
+              title="Resume Roast"
+              description="Resume  Drake, dissing the resume in a personalized and playful manner. Drake will lay down some smooth bars and sharp lyrics highlighting the weaknesses in your resume. After listening to the exceptional track by Toronto's very own, the user is then prompted to fix their resume. They are then treated to yet another musical masterpiece by Drake's biggest rival, Kendrick Lamar. This is where the user gains key insights on specific ways to improve their resume."
+              image="/resumeroast.png"
+              tech="react"/>
+          </section>
         </div>
     </>
   )
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,12 +4,10 @@ export default function Project({ title, image, description, tech}) {
     return (
         <>
         <div id="project-card">
-            <img id="project-image" src={image}/>
+            <img id="project-image" src={image} className='rounded-lg'/>
             <div id="project-title" className='project-box'>{title}</div>
-            <div id="project-info" className='project-box'>
-                <div id="project-description">{description}</div>
-                <div id="project-tech">{tech}</div>
-            </div>
+            <div id="project-description" className='project-box'>{description}</div>
+            <div id="project-tech" className='project-box'>{tech}</div>
         </div>
         </>
     )
