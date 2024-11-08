0629db5545349f72d5b0d9a6821c54c77007ef54
NickrodBasiri <basirinickrod@gmail.com>
2024-11-08 14:02:37 -0500

    removed margin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import '../styles/footer.css';
 
 export default function Footer() {
     return (
-        <div className="flex justify-center items-center gap-10 mb-10 p-10 h-[10vh]">
+        <div className="flex justify-center items-center gap-10 p-10 h-[10vh]">
             <a href="https://github.com/NickrodBasiri" target="_blank"><i className="text-5xl text-white md:text-6xl visited:text-white fa-brands fa-square-github glow"/></a>
             <a href="https://www.linkedin.com/in/nickrodbasiri/" target="_blank"><i className="text-5xl text-white md:text-6xl visited:text-white fa-brands fa-linkedin glow"/></a>
             <a href="mailto:basirinickrod@gmail.com"><i className="text-5xl text-white md:text-6xl visited:text-white fa-envelope fa-solid glow"/></a>
